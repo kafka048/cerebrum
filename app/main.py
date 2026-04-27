fb01fd350bfe1d013578cfb251c68395f1820288
kafka048 <193783320+kafka048@users.noreply.github.com>
2026-04-28 04:57:43 +0530

    feat: registered the routers for the insights

--- app/main.py
+++ app/main.py
@@ -7,6 +7,9 @@ from app.api.goals.router import router as goal_router
 from app.api.tasklogs.router import router as tasklog_router
 from app.api.tasks.router import router as task_router
 
+from app.api.insights.router import router as insights_router
+
+
 Base.metadata.create_all(bind=engine)
 
 
@@ -16,6 +19,9 @@ app.include_router(user_router, prefix="/users", tags=['users'])
 app.include_router(goal_router, prefix="/goals", tags=['goals'])
 app.include_router(task_router, prefix="/tasks", tags=['tasks'])
 app.include_router(tasklog_router, prefix="/tasklogs", tags=['tasklogs'])
+app.include_router(insights_router, tags=['insights'])
+
+
 
 @app.get('/health')
 def health():
